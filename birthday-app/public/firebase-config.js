271f8d1e62081b2e80583cd04ea1ead7a6969c5c
Your Real Name <your-real-email@example.com>
2026-05-20 16:17:54 +0530

    Add Firebase config and VAPID key

---
+++ birthday-app/public/firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAdPod5djCnqz-LkZdPjH9N9h4jDD_E0Fw",
+  authDomain: "birthdayapp-b5eb3.firebaseapp.com",
+  projectId: "birthdayapp-b5eb3",
+  storageBucket: "birthdayapp-b5eb3.firebasestorage.app",
+  messagingSenderId: "377751053783",
+  appId: "1:377751053783:web:cf5d0c2300301fa2f51792",
+  vapidKey: "BHiSFOjot3Gthvw7zXOkC0PaQ4Qe8brc6ASrkJOvagi6RHtvhz58PrYpm4WKWECxev0mOdrjeeXQRFgULpBHzKo"
+}
